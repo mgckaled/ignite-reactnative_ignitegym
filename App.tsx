c1ba6e53692ee48a58dcc63e40ed85c24e074637
mgckaled <marcelgckaled@gmail.com>
2022-12-20 13:49:05 -0300

    feat: config StatusBar

--- App.tsx
+++ App.tsx
@@ -3,12 +3,17 @@ import {
 	Roboto_700Bold,
 	useFonts
 } from '@expo-google-fonts/roboto'
-import { Text, View } from 'react-native'
+import { StatusBar, Text, View } from 'react-native'
 
 export default function App() {
 	const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold })
 	return (
 		<View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+			<StatusBar
+				barStyle="light-content"
+				backgroundColor="transparent"
+				translucent
+			/>
 			{fontsLoaded ? <Text>Hello World</Text> : <View />}
 		</View>
 	)
