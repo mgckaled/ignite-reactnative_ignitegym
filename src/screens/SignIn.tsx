5eb027c139412da47861fdb75c0c612c1afeec5a
mgckaled <marcelgckaled@gmail.com>
2022-12-20 20:34:24 -0300

    feat: create and add Input component to SignIn screen

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -2,10 +2,11 @@ import { Center, Heading, Image, Text, VStack } from 'native-base'
 
 import BackgroundImg from '@assets/background.png'
 import LogoSvg from '@assets/logo.svg'
+import { Input } from '@components/Input'
 
 export function SignIn() {
 	return (
-		<VStack flex={1} bg="gray.700">
+		<VStack flex={1} bg="gray.700" px={10}>
 			<Image
 				source={BackgroundImg}
 				alt="Pessoas treinando"
@@ -23,6 +24,12 @@ export function SignIn() {
 				<Heading color="gray.100" fontSize="xl" mb={6} fontFamily="heading">
 					Acesse a conta
 				</Heading>
+				<Input
+					placeholder="E-mail"
+					keyboardType="email-address"
+					autoCapitalize="none"
+				/>
+				<Input placeholder="Senha" secureTextEntry />
 			</Center>
 		</VStack>
 	)
