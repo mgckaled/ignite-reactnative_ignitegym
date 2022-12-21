2d51fcc0db475605b45abd3145ece1af00473414
mgckaled <marcelgckaled@gmail.com>
2022-12-21 19:26:07 -0300

    feat: add types to app routes

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -1,15 +1,27 @@
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import {
+	BottomTabNavigationProp,
+	createBottomTabNavigator
+} from '@react-navigation/bottom-tabs'
 
-import { Home } from '@screens/Home'
 import { Exercise } from '@screens/Exercise'
 import { History } from '@screens/History'
+import { Home } from '@screens/Home'
 import { Profile } from '@screens/Profile'
 
-const { Navigator, Screen } = createBottomTabNavigator()
+type AppRoutes = {
+	home: undefined
+	exercise: undefined
+	profile: undefined
+	history: undefined
+}
+
+export type AppNavigatorRoutesProps = BottomTabNavigationProp<AppRoutes>
+
+const { Navigator, Screen } = createBottomTabNavigator<AppRoutes>()
 
 export function AppRoutes() {
 	return (
-		<Navigator>
+		<Navigator screenOptions={{ headerShown: false, tabBarShowLabel: false }}>
 			<Screen name="home" component={Home} />
 
 			<Screen name="history" component={History} />
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,7 +1,8 @@
 import { DefaultTheme, NavigationContainer } from '@react-navigation/native'
 import { Box, useTheme } from 'native-base'
 
-import { AuthRoutes } from './auth.routes'
+import { AppRoutes } from './app.routes'
+//import { AuthRoutes } from './auth.routes'
 
 export function Routes() {
 	const { colors } = useTheme()
@@ -11,7 +12,7 @@ export function Routes() {
 	return (
 		<Box flex={1} bg="gray.700">
 			<NavigationContainer theme={theme}>
-				<AuthRoutes />
+				<AppRoutes />
 			</NavigationContainer>
 		</Box>
 	)
