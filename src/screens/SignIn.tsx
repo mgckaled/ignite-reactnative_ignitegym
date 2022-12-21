f735f89f0be51d642178fdca639320415700fc00
mgckaled <marcelgckaled@gmail.com>
2022-12-21 04:48:50 -0300

    feat: create and add Button component

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -2,6 +2,7 @@ import { Center, Heading, Image, Text, VStack } from 'native-base'
 
 import BackgroundImg from '@assets/background.png'
 import LogoSvg from '@assets/logo.svg'
+import { Button } from '@components/Button'
 import { Input } from '@components/Input'
 
 export function SignIn() {
@@ -30,7 +31,9 @@ export function SignIn() {
 					autoCapitalize="none"
 				/>
 				<Input placeholder="Senha" secureTextEntry />
+				<Button title="Acessar" />
 			</Center>
+			<Button title="Criar conta" variant="outline" />
 		</VStack>
 	)
 }
