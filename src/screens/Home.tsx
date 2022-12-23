3c6dafdb3b163f673f7aa1a06507f7d9f7bc7d34
mgckaled <marcelgckaled@gmail.com>
2022-12-23 17:19:20 -0300

    feat: add navigation to History screen

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,9 +1,11 @@
+import { useNavigation } from '@react-navigation/native'
 import { FlatList, HStack, Heading, Text, VStack } from 'native-base'
 import { useState } from 'react'
 
 import { ExerciseCard } from '@components/ExerciseCard'
 import { Group } from '@components/Group'
 import { HomeHeader } from '@components/HomeHeader'
+import { AppNavigatorRoutesProps } from '@routes/app.routes'
 
 export function Home() {
 	const [groups, setGroups] = useState([
@@ -20,6 +22,12 @@ export function Home() {
 		'Levantamento terras'
 	])
 	const [groupSelected, setGroupSelectd] = useState('costas')
+
+	const navigation = useNavigation<AppNavigatorRoutesProps>()
+
+	function handleOpenExerciseDetails() {
+		navigation.navigate('exercise')
+	}
 
 	return (
 		<VStack flex={1}>
@@ -57,7 +65,9 @@ export function Home() {
 				<FlatList
 					data={exercises}
 					keyExtractor={item => item}
-					renderItem={({ item }) => <ExerciseCard />}
+					renderItem={({ item }) => (
+						<ExerciseCard onPress={handleOpenExerciseDetails} />
+					)}
 					showsVerticalScrollIndicator={false}
 					_contentContainerStyle={{
 						paddingBottom: 20
