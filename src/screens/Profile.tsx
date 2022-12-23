551207fc0045200927c7d894dd63ae3c8505e5c3
mgckaled <marcelgckaled@gmail.com>
2022-12-23 15:23:55 -0300

    feat: add userPhoto with Skeleton in Profile screen

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,11 +1,36 @@
-import { VStack } from 'native-base'
+import { Center, ScrollView, Skeleton, VStack } from 'native-base'
+import { useState } from 'react'
 
 import { ScreenHeader } from '@components/ScreenHeader'
+import { UserPhoto } from '@components/UserPhoto'
+
+const PHOTO_SIZE = 33
 
 export function Profile() {
+	const [photoIsLoading, setPhotoIsLoading] = useState(false)
+
 	return (
 		<VStack flex={1}>
 			<ScreenHeader title="Perfil" />
+			<ScrollView>
+				<Center mt={6} px={10}>
+					{photoIsLoading ? (
+						<Skeleton
+							w={PHOTO_SIZE}
+							h={PHOTO_SIZE}
+							rounded="full"
+							startColor="gray.400"
+							endColor="gray.300"
+						/>
+					) : (
+						<UserPhoto
+							source={{ uri: 'https://github.com/mgckaled.png' }}
+							alt="Foto do usuário"
+							size={PHOTO_SIZE}
+						/>
+					)}
+				</Center>
+			</ScrollView>
 		</VStack>
 	)
 }
