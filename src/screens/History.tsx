acf186b2a2013f915584268d43a00a8037a9a7ba
mgckaled <marcelgckaled@gmail.com>
2022-12-23 13:14:12 -0300

    feat: add Section List in History screen

--- src/screens/History.tsx
+++ src/screens/History.tsx
@@ -1,10 +1,34 @@
+import { Heading, SectionList, VStack } from 'native-base'
+import { useState } from 'react'
+
+import { HistoryCard } from '@components/HistoryCard'
 import { ScreenHeader } from '@components/ScreenHeader'
-import { VStack } from 'native-base'
 
 export function History() {
+	const [exercises, setExercises] = useState([
+		{
+			title: '26.08.22',
+			data: ['Puxada frontal', 'Remada unilateral']
+		},
+		{
+			title: '27.08.22',
+			data: ['Puxada frontal']
+		}
+	])
 	return (
 		<VStack flex={1}>
 			<ScreenHeader title="Histórico de Exercícios" />
+			<SectionList
+				sections={exercises}
+				keyExtractor={item => item}
+				renderItem={({ item }) => <HistoryCard />}
+				renderSectionHeader={({ section }) => (
+					<Heading color="gray.200" fontSize="md" mt={10} mb={3}>
+						{section.title}
+					</Heading>
+				)}
+				px={8}
+			/>
 		</VStack>
 	)
 }
