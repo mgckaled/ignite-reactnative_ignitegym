a38a0b8fa411eaf8b9bb7e6f312ad98a628706ad
mgckaled <marcelgckaled@gmail.com>
2022-12-23 15:32:37 -0300

    feat: add inputs for name, email and passowrd in Profile screen

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,9 +1,18 @@
-import { Center, ScrollView, Skeleton, Text, VStack } from 'native-base'
+import {
+	Center,
+	Heading,
+	ScrollView,
+	Skeleton,
+	Text,
+	VStack
+} from 'native-base'
 import { useState } from 'react'
+import { TouchableOpacity } from 'react-native'
 
+import { Button } from '@components/Button'
+import { Input } from '@components/Input'
 import { ScreenHeader } from '@components/ScreenHeader'
 import { UserPhoto } from '@components/UserPhoto'
-import { TouchableOpacity } from 'react-native'
 
 const PHOTO_SIZE = 33
 
@@ -30,6 +39,7 @@ export function Profile() {
 							size={PHOTO_SIZE}
 						/>
 					)}
+
 					<TouchableOpacity>
 						<Text
 							color="green.500"
@@ -41,7 +51,29 @@ export function Profile() {
 							Alterar Foto
 						</Text>
 					</TouchableOpacity>
+
+					<Input bg="gray.600" placeholder="Nome" />
+
+					<Input bg="gray.600" placeholder="E-mail" isDisabled />
 				</Center>
+
+				<VStack px={10} mt={8} mb={9}>
+					<Heading color="gray.200" fontSize="md" mb={2}>
+						Alterar senha
+					</Heading>
+
+					<Input bg="gray.600" placeholder="Senha antiga" secureTextEntry />
+
+					<Input bg="gray.600" placeholder="Nova senha" secureTextEntry />
+
+					<Input
+						bg="gray.600"
+						placeholder="Confirme a nova senha"
+						secureTextEntry
+					/>
+
+					<Button title="Atualizar" mt={4} />
+				</VStack>
 			</ScrollView>
 		</VStack>
 	)
