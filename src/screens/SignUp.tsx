935d140fd89f23703fafb7f6e461469a657df9ff
mgckaled <marcelgckaled@gmail.com>
2022-12-27 20:53:04 -0300

    feat: add schema validation to password and its confirmation

--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -19,7 +19,15 @@ type FormDataProps = {
 
 const signUpSchema = yup.object({
 	name: yup.string().required('Informe o nome'),
-	email: yup.string().required('Informe o e-mail').email('E-mail inválido')
+	email: yup.string().required('Informe o e-mail').email('E-mail inválido'),
+	password: yup
+		.string()
+		.required('Informe a senha')
+		.min(8, 'A senha deve ter pelo menos 8 dígitos.'),
+	password_confirm: yup
+		.string()
+		.required('Confirme a senha.')
+		.oneOf([yup.ref('password'), null], 'A confirmação da senha não confere')
 })
 
 export function SignUp() {
@@ -115,6 +123,7 @@ export function SignUp() {
 								secureTextEntry
 								onChangeText={onChange}
 								value={value}
+								errorMessage={errors.password?.message}
 							/>
 						)}
 					/>
@@ -130,6 +139,7 @@ export function SignUp() {
 								value={value}
 								onSubmitEditing={handleSubmit(handleSignUp)}
 								returnKeyType="send"
+								errorMessage={errors.password_confirm?.message}
 							/>
 						)}
 					/>
