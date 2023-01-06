1c70319088485d2d819f7455209bddc902ee3dc9
mgckaled <marcelgckaled@gmail.com>
2023-01-06 10:15:18 -0300

    [mobile]feat: create api and aply it

--- mobile/src/screens/SignUp.tsx
+++ mobile/src/screens/SignUp.tsx
@@ -8,6 +8,7 @@ import BackgroundImg from '@assets/background.png'
 import LogoSvg from '@assets/logo.svg'
 import { Button } from '@components/Button'
 import { Input } from '@components/Input'
+import { api } from '@services/api'
 
 type FormDataProps = {
 	name: string
@@ -45,16 +46,8 @@ export function SignUp() {
 	}
 
 	async function handleSignUp({ name, email, password }: FormDataProps) {
-		const response = await fetch('http://localhost:3333/users', {
-			method: 'POST',
-			headers: {
-				Accept: 'application/json',
-				'Content-Type': 'application/json'
-			},
-			body: JSON.stringify({ name, email, password })
-		})
-		const data = await response.json()
-		console.log(data)
+		const response = await api.post('/users', { name, email, password })
+		console.log(response.data)
 	}
 
 	return (
