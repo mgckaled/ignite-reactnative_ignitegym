d22ab60d74b1193e7ad6fd7505414f056b72ca94
mgckaled <marcelgckaled@gmail.com>
2023-01-09 17:09:44 -0300

    [mobile]feat: create model context and add it to app.tsx

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -7,6 +7,7 @@ import { NativeBaseProvider } from 'native-base'
 import { StatusBar } from 'react-native'
 
 import { Loading } from '@components/Loading'
+import { AuthContext } from '@contexts/AuthContext'
 import { Routes } from '@routes/index'
 import { THEME } from './src/theme'
 
@@ -19,7 +20,16 @@ export default function App() {
 				backgroundColor="transparent"
 				translucent
 			/>
-			{fontsLoaded ? <Routes /> : <Loading />}
+			<AuthContext.Provider
+				value={{
+					id: '1',
+					name: 'Marcel Kaled',
+					email: 'mgck@gmail.com',
+					avatar: 'rodrigo.png'
+				}}
+			>
+				{fontsLoaded ? <Routes /> : <Loading />}
+			</AuthContext.Provider>
 		</NativeBaseProvider>
 	)
 }
--- mobile/src/routes/index.tsx
+++ mobile/src/routes/index.tsx
@@ -1,11 +1,17 @@
 import { DefaultTheme, NavigationContainer } from '@react-navigation/native'
 import { Box, useTheme } from 'native-base'
+import { useContext } from 'react'
 
 // import { AppRoutes } from './app.routes'
+import { AuthContext } from '@contexts/AuthContext'
 import { AuthRoutes } from './auth.routes'
 
 export function Routes() {
 	const { colors } = useTheme()
+
+	const contextData = useContext(AuthContext)
+
+	console.log('USUÁRIO LOGADO =>', contextData)
 
 	const theme = DefaultTheme
 	theme.colors.background = colors.gray[700]
