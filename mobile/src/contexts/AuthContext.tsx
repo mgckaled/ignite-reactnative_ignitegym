6ff6ffe9fc935af6cb1622e555dfe5ac2d4a3d50
mgckaled <marcelgckaled@gmail.com>
2023-01-15 21:24:06 -0300

    [mobile]feat: load logged user with useEffect

--- mobile/src/contexts/AuthContext.tsx
+++ mobile/src/contexts/AuthContext.tsx
@@ -1,8 +1,9 @@
-import { createContext, ReactNode, useState } from 'react'
+import { createContext, ReactNode, useEffect, useState } from 'react'
 
 import { UserDTO } from '@dtos/UserDTO'
 import { api } from '@services/api'
-import { storageUserSave } from '@storage/storageUser'
+
+import { storageUserGet, storageUserSave } from '@storage/storageUser'
 
 export type AuthContextDataProps = {
 	user: UserDTO
@@ -32,6 +33,18 @@ export function AuthContextProvider({ children }: AuthContextProviderProps) {
 			throw error
 		}
 	}
+
+	async function loadUserData() {
+		const userLogged = await storageUserGet()
+
+		if (userLogged) {
+			setUser(userLogged)
+		}
+	}
+
+	useEffect(() => {
+		loadUserData()
+	}, [])
 
 	return (
 		<AuthContext.Provider value={{ user, singIn }}>
