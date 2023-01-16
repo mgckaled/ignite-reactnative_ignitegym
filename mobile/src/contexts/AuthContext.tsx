c8eb78dd17956a3888894ad866521a33d2947948
mgckaled <marcelgckaled@gmail.com>
2023-01-15 21:34:20 -0300

    [mobile]feat: show loading component as user is loading

--- mobile/src/contexts/AuthContext.tsx
+++ mobile/src/contexts/AuthContext.tsx
@@ -8,6 +8,7 @@ import { storageUserGet, storageUserSave } from '@storage/storageUser'
 export type AuthContextDataProps = {
 	user: UserDTO
 	singIn: (email: string, password: string) => Promise<void>
+	isLoadingUserStorageData: boolean
 }
 
 type AuthContextProviderProps = {
@@ -20,6 +21,8 @@ export const AuthContext = createContext<AuthContextDataProps>(
 
 export function AuthContextProvider({ children }: AuthContextProviderProps) {
 	const [user, setUser] = useState<UserDTO>({} as UserDTO)
+
+	const [isLoadingUserStorageData, setIsLoadingUserStorageData] = useState(true)
 
 	async function singIn(email: string, password: string) {
 		try {
@@ -35,10 +38,16 @@ export function AuthContextProvider({ children }: AuthContextProviderProps) {
 	}
 
 	async function loadUserData() {
-		const userLogged = await storageUserGet()
+		try {
+			const userLogged = await storageUserGet()
 
-		if (userLogged) {
-			setUser(userLogged)
+			if (userLogged) {
+				setUser(userLogged)
+			}
+		} catch (error) {
+			throw error
+		} finally {
+			setIsLoadingUserStorageData(false)
 		}
 	}
 
@@ -47,7 +56,7 @@ export function AuthContextProvider({ children }: AuthContextProviderProps) {
 	}, [])
 
 	return (
-		<AuthContext.Provider value={{ user, singIn }}>
+		<AuthContext.Provider value={{ user, singIn, isLoadingUserStorageData }}>
 			{children}
 		</AuthContext.Provider>
 	)
--- mobile/src/routes/index.tsx
+++ mobile/src/routes/index.tsx
@@ -4,14 +4,20 @@ import { Box, useTheme } from 'native-base'
 import { useAuth } from '@hooks/useAuth'
 import { AppRoutes } from './app.routes'
 import { AuthRoutes } from './auth.routes'
+import { Loading } from '@components/Loading'
 
 export function Routes() {
 	const { colors } = useTheme()
 
-	const { user } = useAuth()
+	const { user, isLoadingUserStorageData } = useAuth()
 
 	const theme = DefaultTheme
 	theme.colors.background = colors.gray[700]
+
+	if (isLoadingUserStorageData) {
+		return <Loading />
+	}
+
 	return (
 		<Box flex={1} bg="gray.700">
 			<NavigationContainer theme={theme}>
