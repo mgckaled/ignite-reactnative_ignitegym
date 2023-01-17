711d4e32c00da55f1b709c56abc29fe0d36fcf75
mgckaled <marcelgckaled@gmail.com>
2023-01-17 11:59:52 -0300

    [mobile]feat: create history dtos and fetch history data in History screen

--- mobile/src/screens/History.tsx
+++ mobile/src/screens/History.tsx
@@ -1,26 +1,53 @@
-import { Heading, SectionList, Text, VStack } from 'native-base'
-import { useState } from 'react'
+import { useFocusEffect } from '@react-navigation/native'
+import { Heading, SectionList, Text, VStack, useToast } from 'native-base'
+import { useCallback, useState } from 'react'
+
+import { HistoryByDayDTO } from '@dtos/HistoryByDayDTO'
+import { api } from '@services/api'
+import { AppError } from '@utils/AppError'
 
 import { HistoryCard } from '@components/HistoryCard'
 import { ScreenHeader } from '@components/ScreenHeader'
 
 export function History() {
-	const [exercises, setExercises] = useState([
-		{
-			title: '26.08.22',
-			data: ['Puxada frontal', 'Remada unilateral']
-		},
-		{
-			title: '27.08.22',
-			data: ['Puxada frontal']
+	const [isLoading, setIsLoading] = useState(true)
+
+	const [exercises, setExercises] = useState<HistoryByDayDTO[]>([])
+
+	const toast = useToast()
+
+	async function fetchHistory() {
+		try {
+			setIsLoading(true)
+			const response = await api.get('/history')
+
+			setExercises(response.data)
+		} catch (error) {
+			const isAppError = error instanceof AppError
+			const title = isAppError
+				? error.message
+				: 'Não foi possível carregar os detalhes do exercício'
+			toast.show({
+				title,
+				placement: 'top',
+				bgColor: 'red.500'
+			})
+		} finally {
+			setIsLoading(false)
 		}
-	])
+	}
+	useFocusEffect(
+		useCallback(() => {
+			fetchHistory()
+		}, [])
+	)
 	return (
 		<VStack flex={1}>
-			<ScreenHeader title="Histórico de Exercícios" />
+			<ScreenHeader title="Histórico" />
+
 			<SectionList
 				sections={exercises}
-				keyExtractor={item => item}
+				keyExtractor={item => item.id}
 				renderItem={({ item }) => <HistoryCard />}
 				renderSectionHeader={({ section }) => (
 					<Heading
@@ -38,11 +65,12 @@ export function History() {
 					exercises.length === 0 && { flex: 1, justifyContent: 'center' }
 				}
 				ListEmptyComponent={() => (
-					<Text color="gray.100" textAlign="center" fontSize="md">
+					<Text color="gray.100" textAlign="center">
 						Não há exercícios registrados ainda. {'\n'}
 						Vamos fazer exercícios hoje?
 					</Text>
 				)}
+				showsVerticalScrollIndicator={false}
 			/>
 		</VStack>
 	)
