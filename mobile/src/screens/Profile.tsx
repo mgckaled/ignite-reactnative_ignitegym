91676d4d696223497237e1121862aae87175fdac
mgckaled <marcelgckaled@gmail.com>
2023-01-23 15:56:52 -0300

    [mobile]feat: add conditions for password validations

--- mobile/src/screens/Profile.tsx
+++ mobile/src/screens/Profile.tsx
@@ -33,7 +33,17 @@ type FormDataProps = {
 }
 
 const profileSchema = yup.object({
-	name: yup.string().required('Informe o nome')
+	name: yup.string().required('Informe o nome'),
+	password: yup
+		.string()
+		.min(6, 'A senha deve ter pelo menos 6 dígitos.')
+		.nullable()
+		.transform(value => (!!value ? value : null)),
+	confirm_password: yup
+		.string()
+		.nullable()
+		.transform(value => (!!value ? value : null))
+		.oneOf([yup.ref('password'), null], 'A confirmação de senha não confere.')
 })
 
 export function Profile() {
