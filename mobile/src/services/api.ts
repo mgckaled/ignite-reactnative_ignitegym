1ed13f866a6042e8870ab262610d5d82913f4fb7
mgckaled <marcelgckaled@gmail.com>
2023-01-24 02:42:29 -0300

    [mobile]feat: get invalid token

--- mobile/src/services/api.ts
+++ mobile/src/services/api.ts
@@ -2,6 +2,8 @@ import axios, { AxiosInstance } from 'axios'
 
 import { AppError } from '@utils/AppError'
 
+import { storageAuthTokenGet } from '@storage/storageAuthToken'
+
 type SignOut = () => void
 
 type APIInstanceProps = AxiosInstance & {
@@ -15,12 +17,18 @@ const api = axios.create({
 api.registerInterceptTokenManager = singOut => {
 	const interceptTokenManager = api.interceptors.response.use(
 		response => response,
-		requestError => {
+		async requestError => {
 			if (requestError.response?.status === 401) {
 				if (
 					requestError.response.data?.message === 'token.expired' ||
 					requestError.response.data?.message === 'token.invalid'
 				) {
+					const oldToken = await storageAuthTokenGet()
+
+					if (!oldToken) {
+						singOut()
+						return Promise.reject(requestError)
+					}
 				}
 
 				singOut()
