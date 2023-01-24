b1eeebcab1df173f5800748741649d01110e8e3e
mgckaled <marcelgckaled@gmail.com>
2023-01-24 02:30:44 -0300

    add token manager to api

--- api/src/middlewares/ensureAuthenticated.js
+++ api/src/middlewares/ensureAuthenticated.js
@@ -1,29 +1,27 @@
-const { verify } = require("jsonwebtoken");
-const AppError = require("../utils/AppError");
-const authConfig = require("../configs/auth");
+const { verify } = require('jsonwebtoken')
+const AppError = require('../utils/AppError')
+const authConfig = require('../configs/auth')
 
 async function ensureAuthenticated(request, response, next) {
-  const authHeader = request.headers.authorization;
+	const authHeader = request.headers.authorization
 
-  if (!authHeader) {
-    throw new AppError("JWT token não informado", 401);
-  }
+	if (!authHeader) {
+		throw new AppError('JWT token não informado', 401)
+	}
 
+	const [, token] = authHeader.split(' ')
 
-  const [, token] = authHeader.split(" ");
+	try {
+		const { sub: user_id } = verify(token, authConfig.jwt.secret)
 
-  try {
+		request.user = {
+			id: Number(user_id)
+		}
 
-    const { sub: user_id } = verify(token, authConfig.jwt.secret);
-
-    request.user = {
-      id: Number(user_id),
-    };
-
-    return next();
-  } catch {
-    throw new AppError("token.invalid", 401);
-  }
+		return next()
+	} catch {
+		throw new AppError('token.expired', 401)
+	}
 }
 
-module.exports = ensureAuthenticated;
+module.exports = ensureAuthenticated
